25e5fef8d1785a63b671fbf8ecf8bc90655ea5eb
Lex Li <support@lextm.com>
2025-12-21 06:27:00 -0500

    Remove theme submodules and update styles; move customization here

--- go.mod
+++ go.mod
@@ -2,4 +2,3 @@ module LEXTUDIO_HOME
 
 go 1.21.7
 
-require github.com/StefMa/hugo-fresh v1.0.0 // indirect
--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
+hugo mod get github.com/StefMa/hugo-fresh@v1.2.0
+hugo mod tidy
+
 hugo server --disableFastRender
